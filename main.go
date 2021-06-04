e1b2a1e4fefbfe947b9534dc266afe56d951754d
Richard Conway <richard@rconway.co.uk>
2021-06-04 22:11:45 +0100

    Add flags for mode (auth/resource) and port

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -9,26 +11,45 @@ import (
 )
 
 func main() {
+	authMode := flag.Bool("auth", false, "use auth mode")
+	resourceMode := flag.Bool("resource", false, "use resource mode")
+	port := flag.Int("port", 8080, "port to listen on")
+	flag.Parse()
+
 	router := mux.NewRouter()
 
-	router.PathPrefix("/auth").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		outcome, ok := r.URL.Query()["outcome"]
-		if ok {
-			code, err := strconv.Atoi(outcome[0])
-			if err == nil {
-				w.WriteHeader(code)
+	// auth mode
+	if *authMode {
+		fmt.Println("auth mode enabled")
+		router.PathPrefix("/auth").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			outcome, haveOutcome := r.URL.Query()["outcome"]
+			if haveOutcome {
+				code, err := strconv.Atoi(outcome[0])
+				if err == nil {
+					w.WriteHeader(code)
+				}
 			}
-		}
 
-		fmt.Fprintf(w, "this is /auth\n")
-		for k, v := range r.URL.Query() {
-			fmt.Fprintf(w, "  %v = %v\n", k, v[0])
-		}
-	})
+			fmt.Fprintf(w, "this is /auth\n")
+			for k, v := range r.URL.Query() {
+				fmt.Fprintf(w, "  %v = %v\n", k, v[0])
+			}
+		})
+	}
+
+	// resource mode
+	if *resourceMode {
+		fmt.Println("resource mode enabled")
+		router.PathPrefix("/resource").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "this is /resource\n")
+		})
+	}
 
 	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "this is root path\n")
 	})
 
-	http.ListenAndServe(":8080", router)
+	addr := fmt.Sprintf(":%v", *port)
+	fmt.Printf("Starting to listen at %v\n", addr)
+	log.Fatal(http.ListenAndServe(addr, router))
 }
