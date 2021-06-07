de6403f446a5e20262c698b1809efd3808f23550
Richard Conway <richard@rconway.co.uk>
2021-06-07 17:10:22 +0100

    Dump request summary to the response

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -31,9 +32,10 @@ func main() {
 			}
 
 			fmt.Fprintf(w, "this is /auth\n")
-			for k, v := range r.URL.Query() {
-				fmt.Fprintf(w, "  %v = %v\n", k, v[0])
-			}
+			// for k, v := range r.URL.Query() {
+			// 	fmt.Fprintf(w, "  %v = %v\n", k, v[0])
+			// }
+			dumpRequest(w, r)
 		})
 	}
 
@@ -42,6 +44,7 @@ func main() {
 		fmt.Println("resource mode enabled")
 		router.PathPrefix("/resource").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "this is /resource\n")
+			dumpRequest(w, r)
 		})
 	}
 
@@ -52,4 +55,34 @@ func main() {
 	addr := fmt.Sprintf(":%v", *port)
 	fmt.Printf("Starting to listen at %v\n", addr)
 	log.Fatal(http.ListenAndServe(addr, router))
+}
+
+func dumpRequest(w http.ResponseWriter, r *http.Request) {
+	// Host
+	fmt.Fprintln(w, "Host:", r.Host)
+	// URL
+	fmt.Fprintln(w, "URL:", r.URL)
+	// Method
+	fmt.Fprintln(w, "Method:", r.Method)
+	// Headers
+	fmt.Fprintln(w, "Headers:")
+	for headerKey, headerVal := range r.Header {
+		fmt.Fprintf(w, "  %v: %v\n", headerKey, headerVal)
+	}
+	// Query params...
+	fmt.Fprintln(w, "Params:")
+	for paramKey, paramVal := range r.URL.Query() {
+		fmt.Fprint(w, "  ", paramKey, ":")
+		for _, paramValItem := range paramVal {
+			fmt.Fprint(w, " ", paramValItem)
+		}
+		fmt.Fprintln(w)
+	}
+	// Body
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("ERROR reading request body:", err)
+	} else {
+		fmt.Fprintln(w, "Body:", string(data))
+	}
 }
